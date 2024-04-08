d8fc7d029ae8aefe9d22bd60c81f3d6759b63b92
Chixol <phw9278@gmail.com>
2024-04-08 11:53:44 +0900

    2024-04-08 11:00

--- basic/src/interaction/StateComponent.tsx
+++ basic/src/interaction/StateComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 
 // 상태(status) : 
 // - 각각의 컴포넌트가 가지는 데이터
@@ -16,6 +16,9 @@ export default function StateComponent() {
     const [count, setCount] = useState<number>(0);
     // let total: number = 0;
     const [total, setTotal] = useState<number>(0);
+    // let favorites: string[] = [];
+    const [favorites, setFavorites] = useState<string[]>(['사과']);
+    const [favorite, setFavorite] = useState<string>('');
 
     const onCountAddHandler = () => {
         // setCount(count + 1);
@@ -39,12 +42,41 @@ export default function StateComponent() {
         setCount(tmp);
         setTotal(total + tmp);
     }
+    // Input 요소의 값을 가져오고자 할땐 onChange 이벤트의 .target.value로 가져옴
+    const onInputHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      // 가져온 .target.value 값을 상태에 지정
+      setFavorite(event.target.value);
+    }
+
+    const onAddListHandler = () => {
+      // 상태가 배열 혹은 객체이면 각각에 대해 요소를 추가, 변경이 일어나도 상태가 변경된 것으로 인식하지 않음 (실제 주소 변경 x)
+      // 상태가 변경된 것도 인식하게 하고 싶으면 새로운 배열 혹은 객체를 생성하여 상태를 변경해야 함 (일반적으로 복사해서 변경)
+
+      // 변경작업은 아래코드가 정석
+      // favorites.push(favorite);
+      // const newFavorites = favorites.map(item => item);
+      // setFavorites(newFavorites);
+
+      // 복사만 할때는 아래코드가 간편
+      setFavorites([...favorites, favorite]);
+      setFavorite('');
+    };
 
     return(
       <>
         <button onClick={onCountAddHandler}>+</button>
         <h1>{count}</h1>
         <h1>{total}</h1>
+
+        <hr />
+        
+        {/* Input이 만약 상태를 변경한다면 value로 그 상태를 지정해야 불일치가 발생하지 않음 */}
+        <input onChange={onInputHandler} value={favorite} />
+        <button onClick={onAddListHandler}>추가</button>
+        <h4>{favorite}</h4>
+        <ul>
+          {favorites.map((item, index) => <li key={index}>{item}</li>)}
+        </ul>
       </>
     )
 }
