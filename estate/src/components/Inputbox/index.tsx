ff940b4632784aa665dd326ad5fa629ee9545496
Chixol <phw9278@gmail.com>
2024-04-11 10:52:06 +0900

    2024-04-11 10:00

--- estate/src/components/Inputbox/index.tsx
+++ estate/src/components/Inputbox/index.tsx
@@ -1,22 +1,24 @@
 //  IdInputBox, PasswordInputBox 함수형 컴포넌트 생성
 // 각각의 컴포넌트는 모두 export 가능하도록 내보냄
-import React from "react";
+import React, { ChangeEvent } from "react";
 import './style.css';
 
 export interface InputBoxProps {
     label: string;
     type: 'text' | 'password';
+    value: string;
     placeholder: string;
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
     buttonTitle?: string;
 }
 
-export default function inputBox({label, type, placeholder, buttonTitle}: InputBoxProps) {
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle}: InputBoxProps) {
 
     return (
         <div className="input-box">
             <div className="input-label label">{label}</div>
             <div className="input-content-box">
-                <input className="input" type={type} placeholder={placeholder} />
+                <input className="input" type={type} value={value} placeholder={placeholder} onChange={onChangeHandler} />
                 { buttonTitle &&
                 <div className="input-disable-button">{buttonTitle}</div>}
             </div>
--- estate/src/views/Authentication/index.tsx
+++ estate/src/views/Authentication/index.tsx
@@ -1,5 +1,9 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import './style.css';
+import SignInBackground from 'assets/image/sign-in-background.png'
+import SignUpBackground from 'assets/image/sign-up-background.png'
+import InputBox from 'components/Inputbox';
+
 
 type AuthPage = 'sign-in' | 'sign-up';
 
@@ -9,13 +13,31 @@ interface Props {
 
 function SignIn ({ onLinkClickHandler }: Props) {
 
-  const onSignInButtonClickHandler = () => {
+    const [id, setId] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+  
+    const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      setId(event.target.value);
+    };
 
-  };
+    const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+      setPassword(event.target.value);
+    };
+
+    const onSignInButtonClickHandler = () => {
+      alert(`아이디 : ${id} / 비밀번호 : ${password}`)
+      setId('');
+      setPassword('');
+    }
+
+
 
   return (
     <div className='authentication-contents'>
-      <div className='authentication-input-container'></div>
+      <div className='authentication-input-container'>
+        <InputBox label="아이디" type="text" value={id} placeholder="아이디를 입력해주세요" onChangeHandler={onIdChangeHandler}/>
+        <InputBox label="비밀번호" type="password" value={password} placeholder="비밀번호를 입력해주세요"  onChangeHandler={onPasswordChangeHandler}/>
+      </div>
       <div className='authentication-button-container'>
         <div className='primary-button full-width' onClick={onSignInButtonClickHandler}>로그인</div>
         <div className='text-link' onClick={onLinkClickHandler}>회원가입</div>
@@ -27,6 +49,8 @@ function SignIn ({ onLinkClickHandler }: Props) {
 }
 
 function SignUp ({onLinkClickHandler}: Props) {
+
+
 
   const onSignUpButtonClickHandler = () => {
 
@@ -56,9 +80,11 @@ export default function Authentication() {
 
   const AuthenticationContents = [page === 'sign-in' ? <SignIn onLinkClickHandler={onLinkClickHandler} /> : <SignUp onLinkClickHandler={onLinkClickHandler}/>]
 
+  const imageBoxStyle = { backgroundImage: `url(${page === 'sign-in' ? SignInBackground : SignUpBackground}` }
+
   return (
     <div id='authentication-wrapper'>
-      <div className='authentication-image-box'></div>
+      <div className='authentication-image-box' style={imageBoxStyle}></div>
         <div className='authentication-box'>
           <div className='authentication-container'>
             <div className='authentication-title h1'>
