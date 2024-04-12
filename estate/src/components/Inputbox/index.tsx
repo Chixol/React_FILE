ea9da9410c1a79d0888364c4fc48840fdac8843b
Chixol <phw9278@gmail.com>
2024-04-12 09:52:05 +0900

    2024-04-12 09:00

--- estate/src/components/Inputbox/index.tsx
+++ estate/src/components/Inputbox/index.tsx
@@ -12,11 +12,14 @@ export interface InputBoxProps {
     buttonTitle?: string;
     buttonStatus?: boolean;
     onButtonClickHandler?: () => void;
+    message?: string;
+    error?: boolean;
 }
 
-export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler}: InputBoxProps) {
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, buttonStatus, onButtonClickHandler, message, error}: InputBoxProps) {
 
     const buttonClass = buttonStatus ? 'input-primary-button' : 'input-disable-button';
+    const messageClass = 'input-message' + (error ? 'error' : 'primary'); 
 
     return (
         <div className="input-box">
@@ -26,7 +29,9 @@ export default function InputBox({label, type, value, placeholder, onChangeHandl
                 { buttonTitle &&
                 <div className={buttonClass} onClick={onButtonClickHandler}>{buttonTitle}</div>}
             </div>
-        <div className="input-message"></div>
+        <div className={messageClass}>
+            {message}
+        </div>
     </div>
     )
 }
--- estate/src/views/Authentication/index.tsx
+++ estate/src/views/Authentication/index.tsx
@@ -82,11 +82,24 @@ function SignUp ({onLinkClickHandler}: Props) {
   const [emailButtonStatus, setEmailButtonStatus] = useState<boolean>(false);
   const [authNumberButtonStatus, setAuthNumberButtonStatus] = useState<boolean>(false);
 
+  const [isIdCheck, setIdCheck] = useState<boolean> (false);
+  const [isEmailCheck, setEmailCheck] = useState<boolean> (false);
+  const [isAuthNumberCheck, setAuthNumberCheck] = useState<boolean> (false);
+
+// 뜻 : 다음과 같은 값이 있어야 회원가입(버튼)이 활성화 된다
+const isSignUpActive = isIdCheck && isEmailCheck && isAuthNumberCheck && password && passwordCheck;
+
+//아래 세개는 모두 같은 동작(코드만 다를뿐 쓰기 나름)
+const signUpButtonClass = isSignUpActive ? 'primary-button full-width' : 'disable-button full-width'
+// const signUpButtonClass =( isSignUpActive ? 'primary' : 'disable');
+// const signUpButtonClass = `${isSignUpActive ? 'primary' : 'disable'}-button full-width'`;
+
 
   const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     const { value } = event.target;
     setId(value);
     setIdButtonStatus(value !== '');
+    setIdCheck(false);
   };
 
   const onPasswordChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
@@ -103,39 +116,48 @@ const onEmailChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
   setEmail(value);
   setEmailButtonStatus(value !== '');
+  setEmailCheck(false);
+  setAuthNumberCheck(false);
 };
 
 const onAuthNumberChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
   const { value } = event.target;
   setAuthNumber(value);
   setAuthNumberButtonStatus(value !== '');
+  setAuthNumberCheck(false);
 };
 
 const onIdButtonClickHandler = () => {
   if(!idButtonStatus) return;
   alert(id);
+  setIdCheck(true);
 };
 
 const onEmailButtonClickHandler = () => {
   if(!emailButtonStatus) return;
   alert(email);
+  setEmailCheck(true);
 };
 
 const onAuthNumberButtonClickHandler = () => {
   if(!authNumberButtonStatus) return;
   alert(authNumber);
+  setAuthNumberCheck(true);
 };
 
-  const onSignUpButtonClickHandler = () => {
+const onSignUpButtonClickHandler = () => {
+  if(!isSignUpActive) return;
+  alert('회원가입');
+};
 
-  };
 
   return (
     <div className='authentication-contents'>
       <SnSContainer title='SNS 회원가입' />
       <div className='short-divider'></div>
       <div className='authentication-input-container'>
-      <InputBox label="아이디" type="text" value={id} placeholder="아이디를 입력해주세요" onChangeHandler={onIdChangeHandler} buttonTitle="중복 확인" buttonStatus={idButtonStatus} onButtonClickHandler={onIdButtonClickHandler} />
+
+      <InputBox label="아이디" type="text" value={id} placeholder="아이디를 입력해주세요" onChangeHandler={onIdChangeHandler} buttonTitle="중복 확인" buttonStatus={idButtonStatus} onButtonClickHandler={onIdButtonClickHandler} error={false} message='사용 가능한 아이디 입니다.'/>
 
       <InputBox label="비밀번호" type="password" value={password} placeholder="비밀번호를 입력해주세요" onChangeHandler={onPasswordChangeHandler} />
 
@@ -143,11 +165,14 @@ const onAuthNumberButtonClickHandler = () => {
 
       <InputBox label="이메일" type="text" value={email} placeholder="이메일을 입력해주세요" onChangeHandler={onEmailChangeHandler} buttonTitle="이메일 인증" buttonStatus={emailButtonStatus} onButtonClickHandler={onEmailButtonClickHandler} />
 
+      { isEmailCheck && 
       <InputBox label="인증번호" type="text" value={authNumber} placeholder="인증번호 4자리를 입력해주세요" onChangeHandler={onAuthNumberChangeHandler} buttonTitle="인증 확인" buttonStatus={authNumberButtonStatus} onButtonClickHandler={onAuthNumberButtonClickHandler} />
+      }
+      
 
       </div>
       <div className='authentication-button-container'>
-        <div className='primary-button full-width' onClick={onSignUpButtonClickHandler}>회원가입</div>
+        <div className={signUpButtonClass} onClick={onSignUpButtonClickHandler}>회원가입</div>
         <div className='text-link' onClick={onLinkClickHandler}>로그인</div>
       </div>
     </div>
