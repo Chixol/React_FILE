4fafdab0e3e7604610a45da4aa9854f49b3b12a5
Chixol <phw9278@gmail.com>
2024-04-17 10:51:58 +0900

    2024-04-17 10:00

--- libraries/src/zustand/ZustandLibrary.tsx
+++ libraries/src/zustand/ZustandLibrary.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { create } from 'zustand';
 
 // zustand 패키지(라이브러리) :
 // - react에서 사용할 수 있는 상태 관리 라이브러리 중 하나
@@ -7,6 +8,32 @@ import React, { useState } from 'react'
 // - Zustand는 단순한 코드 구조와 학습 곡선이 매우 낮음 - useState 사용 수준의 학습 곡선을 요구
 // - Redux, MobX 와 같은 타 상태관리 라이브러리들에 비해 번들의 크기가 가벼움 - 빌드할 때 패키징 속도 향상 / 빌드 후 번들의 크기가 작아짐
 
+// zustand를 이용한 글로벌 상태 선언 방법
+// 1. zustand의 create 함수를 사용하여 store를 생성
+// store : 상태와 상태 관리 로직을 하나로 묶은 객체
+// store 생성하는 create 함수의 반환 데이터는 훅 함수를 반환 (use라는 이름의 함수명으로 받아야함)
+
+// - create 함수의 매개변수로 set 인자를 받는 콜백 함수를 전달해야함
+// - 매개변수로 전달한 콜백 함수는 store 객체를 반환해야함
+// - store 객체는 상태, store 객체를 변경하는 함수가 포함됨
+
+// - typescript에서 zustand의 create 함수를 사용할때는 create 함수의 제너릭으로 store의 타입을 지정해야함
+interface Store {
+    zNormal: number;
+    setZNormal: (zNormal: number) => void;
+    increaseZNormal: () => void;
+    decreaseZNormal: () => void;
+}
+
+// create 함수의 콜백 함수가 받는 set 인자는 상태 변경을 위한 함수
+const useStore = create<Store>((set) => ({
+        zNormal: 0,
+        // set 함수는 매개변수로 현재 상태(state)를 인자로 받는 콜백 함수를 전달해야함
+        // set 함수의 매개변수로 전달된 콜백 함수는 상태객체(store)를 반환해야함
+        setZNormal: (zNormal) => set(state => ({ ...state, zNormal })),
+        increaseZNormal: () => set(state => ({ ...state, zNormal: state.zNormal + 1 })),
+        decreaseZNormal: () => set(state => ({ ...state, zNormal: state.zNormal - 1}))
+    }));
 
 export default function ZustandLibrary() {
 
@@ -25,12 +52,21 @@ export default function ZustandLibrary() {
         setNormal(normal - 1);
     }
 
+    // zustand로 선언한 상태 사용
+    // const { 상태들, 상태 변경 함수들 } = useStoreZustand훅함수()
+    const { zNormal, setZNormal, increaseZNormal, decreaseZNormal } = useStore();
+
     return (
         <div>
             <div>
                 <h4>useState 방식 : {normal}</h4>
                 <button onClick={decreaseNormal}>-</button>
                 <button onClick={increaseNormal}>+</button>
+            </div>
+            <div>
+                <h4>zustand 방식 : {zNormal}</h4>
+                <button onClick={decreaseZNormal}>-</button>
+                <button onClick={increaseZNormal}>+</button>
             </div>
         </div>
     );
